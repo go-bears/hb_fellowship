aa8f1d971bfeb9e8db744399719992e665532556
go-bears <go-bears@users.noreply.github.com>
2016-01-05 17:41:26 -0800

    Create number_guess_plus.py

---
+++ number_guess_plus.py
@@ -0,0 +1,6 @@
+            elif play_again == 'n' or play_again == 'no':
+                print "Thanks for playing!"
+                break
+            else:
+                print "That doesn't make sense. I'm assuming you don't want to play anymore. Bye!"
+                break
