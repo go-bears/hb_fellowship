c7f5cf108ba6c724f701a7bb95ebb2277d0d4772
Hackbright Student <info@hackbrightacademy.com>
2016-01-06 11:35:26 -0800

    fixed calculator functionss, yay!

---
+++ arithmetic.py
@@ -0,0 +1,31 @@
+def add(num1, num2):
+    print num1 + num2
+    return num1 + num2
+
+def subtract(num1, num2):
+    print num1 - num2
+    return num1 - num2
+
+def multiply(num1, num2):
+    print num1 * num2
+    return num1 * num2          
+
+def divide(num1, num2):
+    print float(num1) / num2
+    return float(num1) / num2
+
+def square(num1):
+    print num1 * num1
+    return num1 * num1
+
+def cube(num1):
+    print num1 * num1 * num1
+    return num1 * num1 * num1
+
+def power(num1, num2):
+    print num1 ** num2
+    return num1 ** num2
+
+def mod(num1, num2):
+    print num1 % num2
+    return num1 % num2
