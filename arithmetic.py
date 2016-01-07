eecdf78473c7084f2a9d39164e8757b8c45bec2b
Hackbright Student <info@hackbrightacademy.com>
2016-01-06 16:31:30 -0800

    calculator with added validator

--- arithmetic.py
+++ arithmetic.py
@@ -1,38 +1,4 @@
 def add(num1, num2):
-<<<<<<< HEAD
-    return num1 + num2
-
-
-def subtract(num1, num2):
-    return num1 - num2
-
-
-def multiply(num1, num2):
-    return num1 * num2
-
-
-def divide(num1, num2):
-    # Need to turn at least argument to float for division to
-    # not be integer division
-    return float(num1) / float(num2) 
-
-
-def square(num1):
-    # Needs only one argument
-    return num1 * num1
-
-
-def cube(num1):
-    # Needs only one argument
-    return num1 * num1 * num1
-
-
-def power(num1, num2):
-    return num1 ** num2  # ** = exponent operator
-
-
-def mod(num1, num2):
-=======
     """adds two number together"""
     return num1 + num2
 
@@ -62,5 +28,4 @@ def power(num1, num2):
 
 def mod(num1, num2):
     """returns a remainder"""
->>>>>>> 0d0ef697c41dd5221c3f551572ac9e09a04c0479
     return num1 % num2
--- calculator.py
+++ calculator.py
@@ -11,39 +11,57 @@ from arithmetic import *
 # Your code goes here
 while True:
     #take in user input
-    input = raw_input("> ")
+    try:
+    #if type(num1) and type(num2) == "int" or type(num1) and type(num2) == "float":
+        input = raw_input("Enter a value: ")
+        tokens = input.split(" ")
+        if len(tokens) == 3:
+            command = tokens[0]
+            num1 = int(tokens[1])
+            num2 = int(tokens[2])
+        elif len(tokens) == 2:
+            command = tokens[0]
+            num1 = int(tokens[1])
+        # else:
+        #     raise Exception("You entered too many numbers, please try again.")
+
+
+        
+    except:
+        raise Exception("TypeError, This isn't a number, please enter a valid number")
+        continue
+        
+
     
     #tokenize input
-    tokens = input.split(" ")
     
     # quit command
     if tokens[0] == 'q' or tokens[0]=="Q":
         break
 
-    #operations for two numbers
-    if len(tokens) == 3:
-        command, num1, num2 = tokens
+
+    # #operations for two numbers
+    # if len(tokens) == 3:
+    #     command, num1, num2 = tokens
         
-        if command == '+':
-            print add(int(num1), int(num2))
-        elif command == '-':
-            print subtract(int(num1), int(num2))
-        elif command == "*":
-            print multiply(int(num1), int(num2))
-        elif command == '/':
-            print divide(float(num1), float(num2))
-        elif command == 'pow':
-            print power(int(num1), int(num2)) 
-        elif command == 'mod':
-            print mod(int(num1), int(num2))
+    if command == '+':
+        print add(int(num1), int(num2))
+    elif command == '-':
+        print subtract(int(num1), int(num2))
+    elif command == "*":
+        print multiply(int(num1), int(num2))
+    elif command == '/':
+        print divide(float(num1), float(num2))
+    elif command == 'pow':
+        print power(int(num1), int(num2)) 
+    elif command == 'mod':
+        print mod(int(num1), int(num2))
 
     #operations for one number
-    elif len(tokens) == 2:
-        command, num1 = tokens
-        if command == "square":
-            print square(int(num1))
-        elif command == "cube":
-            print cube(int(num1))
+    if command == "square":
+        print square(int(num1))
+    elif command == "cube":
+        print cube(int(num1))
 
     
 # No setup
