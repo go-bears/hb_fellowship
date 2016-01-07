b4a56960a26ad2b642c54651d9d5f50866b4aed9
Hackbright Student <info@hackbrightacademy.com>
2016-01-06 17:55:23 -0800

    added command validation, length control

--- calculator.py
+++ calculator.py
@@ -8,60 +8,69 @@ calculator program yourself in this file.
 from arithmetic import *
 
 
+
 # Your code goes here
 while True:
-    #take in user input
+    #take in and parse user input
     try:
-    #if type(num1) and type(num2) == "int" or type(num1) and type(num2) == "float":
         input = raw_input("Enter a value: ")
         tokens = input.split(" ")
+        if tokens[0] == 'q' or tokens[0]=="Q":
+            break
+
+        if len(tokens) < 2:
+            command = tokens[0]
+            print "You have too few numbers in a command." 
+
+
         if len(tokens) == 3:
             command = tokens[0]
             num1 = int(tokens[1])
             num2 = int(tokens[2])
         elif len(tokens) == 2:
+            two_items = True
             command = tokens[0]
             num1 = int(tokens[1])
-        # else:
-        #     raise Exception("You entered too many numbers, please try again.")
-
-
+                
+    except ValueError:
+        print "TypeError, This isn't a number, please enter a valid number"
+        # raise Exception("TypeError, This isn't a number, please enter a valid number")
         
-    except:
-        raise Exception("TypeError, This isn't a number, please enter a valid number")
+    
+    #checks input for valid calculator commands    
+    commands_list = ["+", "-", "*", "/", "square", "cube", "pow", "mod"]
+    if command in commands_list:
+        pass
+    
+    else:
+        raise Exception("That was not a valid calculator command. Please try again.")
         continue
-        
 
-    
-    #tokenize input
-    
-    # quit command
-    if tokens[0] == 'q' or tokens[0]=="Q":
-        break
-
-
-    # #operations for two numbers
-    # if len(tokens) == 3:
-    #     command, num1, num2 = tokens
-        
-    if command == '+':
-        print add(int(num1), int(num2))
-    elif command == '-':
-        print subtract(int(num1), int(num2))
-    elif command == "*":
-        print multiply(int(num1), int(num2))
-    elif command == '/':
-        print divide(float(num1), float(num2))
-    elif command == 'pow':
-        print power(int(num1), int(num2)) 
-    elif command == 'mod':
-        print mod(int(num1), int(num2))
+    #calculates operations for two numbers
+    if len(tokens) == 3:    
+        if command == '+':
+            print add(int(num1), int(num2))
+        elif command == '-':
+            print subtract(int(num1), int(num2))
+        elif command == "*":
+            print multiply(int(num1), int(num2))
+        elif command == '/':
+            print divide(float(num1), float(num2))
+        elif command == 'pow':
+            print power(float(num1), float(num2)) 
+        elif command == 'mod':
+            print mod(int(num1), int(num2))
 
     #operations for one number
-    if command == "square":
-        print square(int(num1))
-    elif command == "cube":
-        print cube(int(num1))
+    if len(tokens) == 2:
+        if command == "square":
+            print square(int(num1))
+        elif command == "cube":
+            print cube(int(num1))
+
+    #error message for too many numbers        
+    if len(tokens) > 3:
+        print "You entered too many numbers, please try again."
 
     
 # No setup
