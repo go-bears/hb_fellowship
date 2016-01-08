14e6e4951bc46269c4539232e377a0d600fbe494
go-bears <go-bears@users.noreply.github.com>
2016-01-07 19:00:01 -0800

    Update calculator.py
    refactor to add functions

--- calculator.py
+++ calculator.py
@@ -8,82 +8,94 @@ calculator program yourself in this file.
 from arithmetic import *
 
 
-
 # Your code goes here
-while True:
-    #take in and parse user input
-    input = raw_input(">> ")
+
+
+
+def parse_input(input):
+    """tokenizes user input, outputs command, number_list"""
+
     tokens = input.split(" ")
     command = tokens[0]
     number_list = tokens[1:]
+    tokenized_input = command, number_list
+    return tokenized_input
 
-    #quit command
-    if tokens[0] == 'q' or tokens[0]=="Q":
-        break
+def quit_calculator(tokenized_input): 
+    """checks for quit command"""
+    command, number_list = tokenized_input
+    if command == 'q' or command=="Q":
+        return False
 
-    # float_list = []
-    # for item in number_list:
-    #     item = float(item)
-    #     float_list.append(item)
-
-    #checks input for valid numbers    
+def validates_number_input(tokenized_input):
+    """checks input for valid numbers"""
+    command, number_list = tokenized_input
     try:
         number_list = [int(i) for i in number_list]
+        return number_list
 
     except ValueError:
         print "TypeError, This isn't a number, please enter a valid number"
 
     try:
         float_list = [float(i) for i in number_list]
+        return float_list
+
     except ValueError:
         print "TypeError, This isn't a number, please enter a valid number"
 
-    
-    #checks input for valid calculator commands    
+
+def validates_commands(tokenized_input):
+    """checks input for valid calculator commands"""
+    command, number_list = tokenized_input
     try:        
         commands_list = ["+", "-", "*", "/", "square", "cube", "pow", "mod"]
         if command in commands_list:
-            pass    
+            return command    
     except:
         print Exception("That was not a valid calculator command. Please try again.")
-        
+    
 
-    #calculates operations        
+def calculator_operations(tokenized_input):
+    """performs math operations with imported methods from arithmetic.py"""
+    command, number_list = tokenized_input
+
     if command == '+':
         result = reduce(lambda num1, num2: add(num1, num2), number_list) 
-        print result
-
+        return result
     elif command == '-':
         result = reduce(lambda num1, num2: subtract(num1, num2), number_list)
-        print result
+        return result
     elif command == "*":
         result = reduce(lambda num1, num2: multiply(num1, num2), number_list)
-        print result
+        return result
     elif command == '/':
         result = reduce(lambda num1, num2: divide(num1, num2), float_list)
-        print result
+        return result
 
     elif command == 'pow':
         result = reduce(lambda num1, num2: power(num1, num2), float_list)
-        print power(float(num1), float(num2)) 
+        return power(float(num1), float(num2)) 
     elif command == 'mod':
         result = reduce(lambda num1, num2: mod(num1, num2), number_list)
-        print result
+        return result
 
-    #operations for one number *use reduce function to reduce to single value
+    #operations for one number; uses reduce function to reduce number_list to a single value
     if command == "square":
         result = reduce(lambda num1, num2: add(num1, num2), number_list)
-        print square(result)
+        return square(result)
         
     elif command == "cube":
         result = reduce(lambda num1, num2: add(num1, num2), number_list)
-        print cube(result)
+        return cube(result)
 
- 
-    
-# No setup
-# repeat forever:
-#     read input
-#     tokenize input
-#     if the first token is 'q', quit
-#     otherwise decide which math function to call based on the tokens we read
+def main():
+    while True:
+        input = raw_input(">> ")
+        tokenized_input = parse_input(input)
+        quit_calculator(tokenized_input)
+        validates_number_input(tokenized_input)
+        validates_commands(tokenized_input)
+        calculator_operations(tokenized_input)
+
+main()
